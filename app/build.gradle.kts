e3f84c89f20b1f73c3004885a9f1347f2d2a6bc4
Naveed Hassan <hassan242135@gmail.com>
2025-11-14 10:21:22 +0500

    feat: bump version code and version name to 15 and 1.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "net.invictusmanagement.invictuskiosk"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.14"
+        versionCode = 15
+        versionName = "1.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
