9e9147f967f37ccfbdb03e3145cc5180232df7c6
Naveed Hassan <hassan242135@gmail.com>
2025-11-14 14:02:07 +0500

    feat: update version code and name to 1.16; modify deployment target selection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "net.invictusmanagement.invictuskiosk"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.15"
+        versionCode = 16
+        versionName = "1.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
